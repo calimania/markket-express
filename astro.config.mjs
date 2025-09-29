3b30065a71bff9cfb236084d5a618aeccb7b147d
Daveed <daveed@santocabron.com>
2025-09-29 14:35:17 -0400

    Configuring markket in the content layer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import react from "@astrojs/react";
 
+const PUBLIC_URL = process.env.PUBLIC_URL || 'https://express.markket.place';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+  site: PUBLIC_URL,
+  base: '/'
 });
